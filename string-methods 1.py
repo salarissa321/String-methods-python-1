f4e2bea9d95f2bf342f504ce8823beed26048b5c
Salar Issa <120121093+salarissa321@users.noreply.github.com>
2023-04-19 12:28:25 +0200

    Update string-methods 1.py

--- string-methods 1.py
+++ string-methods 1.py
@@ -8,50 +8,51 @@
 #split()  rsplit()
 
 a = " I Love Python and PHP and mySQL"
-print(a.split())
+print(a.split()) # ['I', 'Love', 'Python', 'and', 'PHP', 'and', 'mySQL']
 
 b = " I-Love-Python-and-PHP-and-mySQl"
-print(b.split("-"))
+print(b.split("-")) # [' I', 'Love', 'Python', 'and', 'PHP', 'and', 'mySQl']
 
 
 c = " I-Love-Python-and-PHP-and-mySQL"
-print(c.split("-" , 3))
+print(c.split("-" , 3)) # [' I', 'Love', 'Python', 'and-PHP-and-mySQL']
 
 d = " I-Love-Python-and-PHP-and-mySQL"
-print(d.rsplit("-" , 3))
+print(d.rsplit("-" , 3)) # [' I-Love-Python-and', 'PHP', 'and', 'mySQL']
 
 # Center()
 
 s= "Salar"
-print(s.center(9))
-print(s.center(9 , "#"))
-print(s.center(15 , "!"))
+print(s.center(9)) #   Salar 
+print(s.center(9 , "#")) ###Salar##
+print(s.center(15 , "!")) # !!!!!Salar!!!!!
 
 #count()
 
 f = " I Love Python and PHP because PHP is Easy"
-print(f.count("PHP"))
-print(f.count("PHP" ,0 ,35))
+print(f.count("PHP")) # 2
+print(f.count("PHP" ,0 ,35)) # 2
 
 # swapcase()
 p = " I love python"
 d = " i LOVE pYTHON"
 
-print(p.swapcase())
-print(d.swapcase())
+print(p.swapcase()) #  i LOVE PYTHON
+print(d.swapcase()) # I love Python
 
 # Startswith
 p = "I love Python"
-print(p.startswith("I"))
-print(p.startswith("s"))
-print(p.startswith("p" , 6, 12))
+print(p.startswith("I")) # True
+print(p.startswith("s")) # False
+print(p.startswith("p" , 6, 12)) # False
 
 
 # Endwith
 o = "I love Python"
-print(o.endswith("n"))
-print(o.endswith("s"))
-print(o.endswith("e" , 2, 6))
+print(o.endswith("n")) # True
+print(o.endswith("s")) # False
+print(o.endswith("e" , 2, 6)) # True
+
 
 
 
